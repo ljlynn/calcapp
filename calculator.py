0b7a0a5b3c3144dbb093bf2113d2089708395d05
Logan <ljlynn@gmail.com>
2020-05-03 23:30:36 -0500

    added return_json method and unit tests

--- calculator.py
+++ calculator.py
@@ -11,11 +11,19 @@ class Calculator():
         '''
         Constructor for the Calculator service. Numbers used by calulator are stored here.
         '''
-        self.first_number = first_number
-        self.second_number = second_number
+        self.first_number = int(first_number)
+        self.second_number = int(second_number)
 
     def add_numbers(self):
         '''
         This method takes two numbers and adds them together.
         '''
         return self.first_number + self.second_number
+
+    def return_json(self):
+        obj_sum = self.add_numbers()
+        return {
+            'first_number': self.first_number,
+            'second_number': self.second_number,
+            'sum': obj_sum
+        }
--- test_calculator.py
+++ test_calculator.py
@@ -13,3 +13,9 @@ def test_add_two_numbers():
     assert calc_obj1.add_numbers() == 4
     calc_obj2 = calculator.Calculator(1, -1)
     assert calc_obj2.add_numbers() == 0
+
+def test_return_json():
+    calc_obj3 = calculator.Calculator(2, 2)
+    json_obj = calc_obj3.return_json()
+
+    assert json_obj['sum'] == 4
