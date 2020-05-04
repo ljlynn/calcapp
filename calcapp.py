d70ff3320405f20197d57f302fdca8a04e179a95
Logan <ljlynn@gmail.com>
2020-05-04 00:05:08 -0500

    added sum feature to website

--- calcapp.py
+++ calcapp.py
@@ -1,9 +1,10 @@
 '''
 This app will display a form with two fields to allow the user to enter two numbers and get the sum 
-of those numbers. 
+of those numbers. This application is unit tested!
 '''
 
 from flask import Flask, render_template, request
+import calculator
 
 app = Flask(__name__, template_folder='templates')
 
@@ -13,9 +14,12 @@ def calc():
     Controller code for the main webesite application
     '''
     if request.method == 'POST':
-        pass
-    else:
-        return render_template('calculator.html')
+        calc_obj = calculator.Calculator(request.form['first_number'],
+                                           request.form['second_number'])
+        calc_json = calc_obj.return_json()
+
+        return render_template('calculator.html', results=calc_json)
+    return render_template('calculator.html')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
--- test_system.py
+++ test_system.py
@@ -11,3 +11,28 @@ def test_website_load():
     get_headers = {"Accpet": "text/html"}
     site_resp = requests.get('http://localhost:5000', headers=get_headers)
     assert site_resp.status_code == 200
+
+def test_get_bad_page():
+    '''
+    Simulate loading a page that doesn't exist
+    '''
+    get_headers = {"Accpet": "text/html"}
+    get_resp = requests.get(
+        "http://localhost:5000/bad",
+        headers=get_headers
+    )
+    assert get_resp.status_code == 404
+
+def test_submit_form():
+    post_headers = {
+        "Accept": "text/html",
+        "Content-Type": "application/x-www-form-urlencoded",
+    }
+
+    post_resp = requests.post(
+        "http://localhost:5000",
+        headers=post_headers,
+        data="first_number=2&second_number=2"
+    )
+    assert post_resp.status_code == 200
+    assert "The sum of 2 and 2 is 4" in post_resp.text
